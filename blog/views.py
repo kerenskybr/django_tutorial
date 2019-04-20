8f0b30d3c11d198c3955ca53481a7c9ff51ef49d
kerenskybr <roger.o.monteiro@gmail.com>
2019-04-20 19:22:29 -0300

    Add boostrap

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
 
+posts = [
+	{
+		'author': 'Roger',
+		'title': 'Blog Post 1',
+		'content': 'Conteudo do post',
+		'date_posted': 'August 27, 2018'
+	},
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'Conteudo do post2',
+		'date_posted': 'August 28, 2018'
+	}
+]
+
+
 # Create your views here.
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return render(request, 'blog/about.html')
+	return render(request, 'blog/about.html', {'title' : 'About Page'})
