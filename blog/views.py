eba20dd74da407351d313a4fe18b7462c8464069
kerenskybr <roger.o.monteiro@gmail.com>
2019-04-28 14:32:45 -0300

    Admin page and migrations done

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 
-posts = [
-	{
-		'author': 'Roger',
-		'title': 'Blog Post 1',
-		'content': 'Conteudo do post',
-		'date_posted': 'August 27, 2018'
-	},
-	{
-		'author': 'Jane Doe',
-		'title': 'Blog Post 2',
-		'content': 'Conteudo do post2',
-		'date_posted': 'August 28, 2018'
-	}
-]
-
-
-# Create your views here.
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
